437163922d1915e2c499ecfc8bee628ef4e7f144
ChivisAthisa <monserga975@gmail.com>
2022-03-02 11:08:12 -0600

    commit 020322

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent  {
+
+  @ViewChild('txtBuscar') txtBuscar:ElementRef<HTMLInputElement>;
+
+  constructor(private GifsService:GifsService){}
+
+   buscar(){
+     const valor = this.txtBuscar.nativeElement.value;
+     
+     /* Si la info esta en blanco, no se debe agregar */
+     if (valor.trim().length === 0 ){ return; }
+
+     this.GifsService.buscarGifs(valor);
+     
+     this.txtBuscar.nativeElement.value = '';
+   }
+
+  /* F2 buscar( termino:string ){
+    console.log( this.txtBuscar);
+  } */
+
+ /* F1 buscar(event:KeyboardEvent){
+   if (event.key === 'Enter'){
+     console.log(event);
+   }
+ } */
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent  {
+
+  constructor(private GifsService:GifsService){}
+
+  get historial(){
+    /* return [...this.GifsService.historial]; */
+    return this.GifsService.historial;
+  }
+
+  buscar(termino: string){
+    this.GifsService.buscarGifs(termino);
+  }
+
+  reiniciar(){
+    this.GifsService.reiniciarBusqueda();
+  }
+
+}
